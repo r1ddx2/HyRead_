025d560084c383b8f517d0e3963148fa1009143e
Red Wang <redxoxored@gmail.com>
2024-03-21 01:51:07 +0800

    feat - Add `Book` data model

---
+++ HyRead/Model/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  HyRead
+//
+//  Created by Red Wang on 2024/3/21.
+//
+
+import Foundation
+
+struct Book: Codable {
+    let uuid: Int
+    let title: String
+    let coverUrl: String
+    let publishDate: String
+    let publisher: String
+    let author: String
+    var isFavorite: Bool?
+}
