e5fa575add114fbe1ed86e77ae0a720d6f9e9395
Red Wang <redxoxored@gmail.com>
2024-03-22 17:17:37 +0800

    feat - Add `BooksManager()` to manage methods and decouple viewModels with `StorageManager()` and `APIManager()`

--- HyRead/Manager/BooksManager.swift
+++ HyRead/Manager/BooksManager.swift
@@ -6,11 +6,44 @@
 //
 
 import UIKit
+import Combine
 
 class BooksManager {
     
+    private var cancellables = Set<AnyCancellable>()
+    
+    private let apiManager = APIManager.shared
+    private let storageManager = StorageManager.shared
+    
+    // MARK: - Methods
+    // Fetch API
     func fetchBooks() -> BookPublisher {
-        return APIManager.shared.fetchData()
+        return apiManager.fetchData()
+            .eraseToAnyPublisher()
+    }
+    
+    // Load Core Data
+    func loadBooks() {
+        storageManager.loadAllData()
+    }
+    
+    // Save to Core Data
+    func saveToStorage(_ books: [Book]) {
+        for book in books {
+            storageManager.updateData(book: book)
+        }
+        loadBooks()
+    }
+    
+    // Update favorite
+    func updateFavorite(uuid: Int) {
+        storageManager.updateFavorite(for: uuid)
+    }
+    
+    // Pass on to viewModel
+    func bookUpdates() -> BookNeverPublisher {
+        return storageManager.$bookList
             .eraseToAnyPublisher()
     }
 }
+
