e21304ca798d24b54583db7da06798153e9bbb7d
Red Wang <redxoxored@gmail.com>
2024-03-22 17:21:20 +0800

    feat - Bind with `BookManager` and perform methods from manager

--- HyRead/ViewModel/BooksViewModel.swift
+++ HyRead/ViewModel/BooksViewModel.swift
@@ -8,32 +8,50 @@
 import UIKit
 import Combine
 
-class BooksViewModel: ObservableObject {
-    private let bookManager = BooksManager()
+
+typealias BookPublisher = AnyPublisher<[Book], Error>
+typealias BookNeverPublisher = AnyPublisher<[Book], Never>
+
+class BooksViewModel {
+    
+    private let bookManager: BooksManager!
     private var cancellables = Set<AnyCancellable>()
+    
     @Published private(set) var bookList = [Book]()
+    
+    init(bookManager: BooksManager) {
+        self.bookManager = bookManager
+        bindBookUpdates()
+    }
+    
+    // MARK: - Methods
+    func bindBookUpdates() {
+        bookManager.bookUpdates()
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.bookList, on: self)
+            .store(in: &cancellables)
+    }
     
     func fetchBooks() {
         
         bookManager.fetchBooks()
             .receive(on: DispatchQueue.main)
-            .sink { completion in       // Handle error
-                if case let .failure(error) = completion {
+            .sink { [weak self] result in
+                if case let .failure(error) = result {
+                    // Load Core Data
+                    self?.bookManager.loadBooks()
                     print("🔴Failed to fetch books: \(error.localizedDescription)")
                 }
-            } receiveValue: { [weak self] books in      // Handle success
+            } receiveValue: { [weak self] books in
                 self?.bookList = books
-                print("🟢Received books")
+                // Save to Core Data
+                self?.bookManager.saveToStorage(books)
+                print("🟢Success fetched books")
             }
             .store(in: &cancellables)
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
+    func updateFavorite(for uuid: Int) {
+        bookManager.updateFavorite(uuid: uuid)
+    }
 }
